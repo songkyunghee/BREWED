2aab3eecff6ab8165c951e9d9ac44a56c8eff778
송경희 <kyunghee794613@gmail.com>
2021-11-19 16:00:02 +0900

    OrderFragment 뷰 구현

--- android_admin/mobile_cafe_admin_fianl_project/app/src/main/java/com/ssafy/mobile_cafe_admin_fianl_project/src/main/fragment/LoginFragment.kt
+++ android_admin/mobile_cafe_admin_fianl_project/app/src/main/java/com/ssafy/mobile_cafe_admin_fianl_project/src/main/fragment/LoginFragment.kt
@@ -2,56 +2,43 @@ package com.ssafy.mobile_cafe_admin_fianl_project.src.main.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.mobile_cafe_admin_fianl_project.R
+import com.ssafy.mobile_cafe_admin_fianl_project.databinding.FragmentLoginBinding
 import com.ssafy.mobile_cafe_admin_fianl_project.databinding.FragmentOrderBinding
+import com.ssafy.mobile_cafe_admin_fianl_project.src.main.activity.LoginActivity
 import com.ssafy.mobile_cafe_admin_fianl_project.src.main.activity.MainActivity
 import com.ssafy.mobile_cafe_admin_fianl_project.src.main.adapter.OrderListAdapter
 
 
-class OrderFragment : Fragment() {
-    private lateinit var binding: FragmentOrderBinding
-    private lateinit var mainActivity: MainActivity
-    private lateinit var orderListAdapter: OrderListAdapter
-    private val list = mutableListOf<Int>()
+class LoginFragment : Fragment() {
+    private lateinit var loginActivity: LoginActivity
+    lateinit var binding: FragmentLoginBinding
+    lateinit var btnLogin : Button
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        mainActivity = context as MainActivity
+        loginActivity = context as LoginActivity
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentOrderBinding.inflate(inflater, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        list.add(R.drawable.coffee1)
-        list.add(R.drawable.coffee1)
-        list.add(R.drawable.coffee1)
-        list.add(R.drawable.coffee1)
-
-        orderListAdapter = OrderListAdapter(requireContext(), list)
-
-        binding.recyclerViewOrder.apply {
-            val linearLayoutManager = LinearLayoutManager(context)
-            linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
-            layoutManager = linearLayoutManager
-            adapter = orderListAdapter
-
-            adapter!!.stateRestorationPolicy =
-                RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+        btnLogin = view.findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+            loginActivity.openFragment(1)
         }
 
     }
