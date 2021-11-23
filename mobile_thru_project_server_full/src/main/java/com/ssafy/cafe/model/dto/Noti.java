adde30931f62d4fa4839edcad73ee759e192fd4e
송경희 <kyunghee794613@gmail.com>
2021-11-23 20:59:35 +0900

    서버 noti 작업 중

---
+++ mobile_thru_project_server_full/src/main/java/com/ssafy/cafe/model/dto/Noti.java
@@ -0,0 +1,45 @@
+package com.ssafy.cafe.model.dto;
+
+import java.util.Date;
+
+public class Noti {
+	private Integer nId;
+	private String nMsg;
+	private Date notiTime;
+	private String userId;
+	
+	public Noti(Integer nId, String nMsg, Date notiTime, String userId) {
+		this.nId = nId;
+		this.nMsg = nMsg;
+		this.notiTime = notiTime;
+		this.userId = userId;
+	}
+	
+	public Integer getnId() {
+		return nId;
+	}
+	public void setnId(Integer nId) {
+		this.nId = nId;
+	}
+	public String getnMsg() {
+		return nMsg;
+	}
+	public void setnMsg(String nMsg) {
+		this.nMsg = nMsg;
+	}
+	public Date getNotiTime() {
+		return notiTime;
+	}
+	public void setNotiTime(Date notiTime) {
+		this.notiTime = notiTime;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+}
--- mobile_thru_project_server_full/src/main/java/com/ssafy/cafe/model/service/UserServiceImpl.java
+++ mobile_thru_project_server_full/src/main/java/com/ssafy/cafe/model/service/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ssafy.cafe.model.service;
 
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ssafy.cafe.model.dao.UserDao;
+import com.ssafy.cafe.model.dto.Noti;
 import com.ssafy.cafe.model.dto.User;
 
 /**
@@ -55,4 +57,24 @@ public class UserServiceImpl implements UserService {
 		return userDao.update(user);
 		
 	}
+
+	@Override
+	public List<Map<String, Object>> selectNotiByUser(String id) {
+		// TODO Auto-generated method stub
+		return userDao.selectNotiByUser(id);
+	}
+
+	@Override
+	public int deleteNoti(int n_id) {
+		// TODO Auto-generated method stub
+		return userDao.deleteNoti(n_id);
+	}
+
+	@Override
+	public int insertNoti(Noti noti) {
+		// TODO Auto-generated method stub
+		return userDao.insert(noti);
+	}
+	
+	
 }
